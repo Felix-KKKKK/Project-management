5b9222d77c2563a3e0b3d1d11bc287bfbf71a772
emtsa0519 <em20081114@gmail.com>
2025-05-14 11:34:39 -0400

    Update ProjectManagement.java

--- src/yiprojectmanagement/ProjectManagement.java
+++ src/yiprojectmanagement/ProjectManagement.java
@@ -1,15 +1,9 @@
-//
+
 package yiprojectmanagement;
 
-/**
- *
- * @author felix
- */
 public class YiProjectManagement {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         
     }
