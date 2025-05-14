cd7babc1c0bacb040d5e231e97600865fc0970e2
iamwilsonx1 <121518802+iamwilsonx1@users.noreply.github.com>
2025-05-14 11:07:17 -0400

    Update YiProjectManagement.java

--- src/yiprojectmanagement/YiProjectManagement.java
+++ src/yiprojectmanagement/YiProjectManagement.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package yiprojectmanagement;
-//
+
 /**
  *
  * @author felix
@@ -14,7 +14,7 @@ public class YiProjectManagement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
